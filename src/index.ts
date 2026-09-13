a59ef839a86e83faf6513ed8a51aa4b13ce0fba4
AzureToast <kuyajc670@gmail.com>
2026-09-13 14:59:54 -0600

    app will now check for duplicate instances

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app } from 'electron';
+import { app, dialog } from 'electron';
 import { init as configManagerInit} from './app/configManager';
 import { init as obsManagerInit} from './app/obsManager';
 import { init as appManagerInit } from './app/appManager';
@@ -6,16 +6,23 @@ import { init as trayManagerInit } from './app/trayManager';
 import { init as loggerManagerInit } from './app/loggerManager';
 import * as statics from './statics';
 
-try {
-    app.whenReady().then(() => {
-        console.log(`OBS Replay Tool v${statics.version} by ${statics.author} (${statics.authorUrl})`);
-        configManagerInit();
-        obsManagerInit();
-        appManagerInit();
-        trayManagerInit();
-        loggerManagerInit();
-    });
-} catch (err) {
-    console.error(err);
-    process.exit(1);
+if (!app.requestSingleInstanceLock()) {
+    app.whenReady().then(async () => {
+        dialog.showErrorBox('Duplicate Instance Detected', 'OBS replay tool is already running, you can only run one instances of this app at a time. App will now close.')
+    })
+    app.quit();
+} else {
+    try {
+        app.whenReady().then(() => {
+            console.log(`OBS Replay Tool v${statics.version} by ${statics.author} (${statics.authorUrl})`);
+            configManagerInit();
+            obsManagerInit();
+            appManagerInit();
+            trayManagerInit();
+            loggerManagerInit();
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 }
